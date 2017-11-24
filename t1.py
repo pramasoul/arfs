d7043c8819a1d8226deca19b831c8e6b68c86beb
Tom Soulanille <soul@prama.com>
2017-11-23 20:20:30 -0800

    ArF, Volume

--- t1.py
+++ t1.py
@@ -2,6 +2,33 @@
 # just sketching - tas
 
 
+import base64
+import hashlib
+
+class ArF:
+    def __init__(self, f):
+        self.f = f
+
+    def __getattr__(self, k):
+        if 'k' in self.__dict__:
+            return self.k
+        self.k = self.keyOf(self.f)
+        return self.k
+
+    def keyOf(self, f):
+        m = hashlib.sha256()
+        buf = bytearray(4096)
+        pos = f.seek(0, 1)
+        f.seek(0)
+        n = f.readinto(buf)
+        while n == len(buf):
+            m.update(buf)
+            n = f.readinto(buf)
+        m.update(buf[:n])
+        f.seek(pos)
+        return base64.urlsafe_b64encode(m.digest())
+
+
 class Index:
     def __init__(self):
         self.index = dict()
@@ -16,14 +43,39 @@ class Index:
         self.index[name] = value
 
 
+class Volume:
+    def __init__(self, arfile):
+        self.arfile = arfile
+
+    def append(self, k, f):
+        buf = bytearray(4096)
+        arf = self.arfile
+        start = arf.seek(0,2)
+        length = 0
+        n = f.readinto(buf)
+        while n == len(buf):
+            arf.write(buf)
+            length += n
+        arf.write(buf[:n])
+        length += n
+        return start, length
+
+    def flush(self):
+        self.arfile.flush()
+
 
 class Archive:
     def __init__(self, arfile):
         self.arfile = arfile
+        self.ix = Index()
+        self.vol = Volume(arfile)
 
     def include(self, f):
         # Make f included in archive
-        raise NotImplementedError
+        ffa = ArF(f)
+        if ffa.k in self.ix:
+            return
+        self.vol.append(k, f)
 
     def get(self, name):
         # Return a file from name
@@ -33,3 +85,10 @@ class Archive:
         # Is name in archive?
         raise NotImplementedError
 
+
+def foo(f):
+    ffa = ArF(f)
+    a.include(ffa)
+
+def bar(name):
+    a.get(name)
--- test_t1.py
+++ test_t1.py
@@ -1,8 +1,37 @@
 import unittest
 from unittest import mock
 from random import randrange
+import io
+from base64 import b64decode
+from binascii import hexlify
 
-from t1 import Archive, Index
+from t1 import Archive, ArF, Index, Volume
+
+class TestArF(unittest.TestCase):
+    def setUp(self):
+        self.f = io.BytesIO(b"The quick brown fox jumped over the lazy dog.")
+        self.a = ArF(self.f)
+        # echo -n 'The quick brown fox jumped over the lazy dog.' | openssl dgst -sha25
+        self.hexdigest = b'68b1282b91de2c054c36629cb8dd447f12f096d3e3c587978dc2248444633483'
+
+    def testInit(self):
+        self.assertIsInstance(self.a, ArF)
+
+    def testKey(self):
+        k = self.a.k
+        self.assertEqual(hexlify(b64decode(k)), self.hexdigest)
+
+
+    def testKeyKeepPos(self):
+        k = self.a.k
+        self.assertEqual(self.f.read(), b"The quick brown fox jumped over the lazy dog.")
+
+    def testKeyKeepPos(self):
+        self.f.read(10)
+        k = self.a.k
+        self.assertEqual(hexlify(b64decode(k)), self.hexdigest)
+        self.assertEqual(self.f.read(), b"The quick brown fox jumped over the lazy dog."[10:])
+
 
 class TestIndex(unittest.TestCase):
     def testInit(self):
@@ -27,6 +56,25 @@ class TestIndex(unittest.TestCase):
         ix.record(b'foo', b'bar')
         self.assertTrue(ix.has(b'foo'))
 
+
+class TestVolume(unittest.TestCase):
+    def setUp(self):
+        self.arfile = io.BytesIO()
+        self.vol = Volume(self.arfile)
+
+    def testInit(self):
+        self.assertIsInstance(self.vol, Volume)
+
+    def testAppend(self):
+        vol = self.vol
+        s, l = vol.append(b'fookey', io.BytesIO(b'foo'))
+        self.assertEqual(s, 0)
+        self.assertEqual(l, 3)
+        self.assertEqual(self.arfile.getvalue(), b'foo')
+        s, l = vol.append(b'barkey', io.BytesIO(b'bartleby'))
+        self.assertEqual(s, 3)
+        self.assertEqual(l, 8)
+        self.assertEqual(self.arfile.getvalue(), b'foobartleby')
 
 class TestArchive(unittest.TestCase):
     def testInit(self):
